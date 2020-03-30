494595b80ca74496355317d53832d4c4b9d30e91
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-03-30 16:31:20 -0400

    done what I can

--- main.js
+++ main.js
@@ -1,13 +1,73 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const firstNumberIsLess = (num1,num2) => {
+  if(num1 > num2){//i can also use num1-num2 but this helps me understand//
+return 1;
+  }else if (num1 < num2) {
+    return -1;
+  }
+  return 0;
+}
 
+const secondNumberIsLess = (num1, num2) => {
+  if(num1 > num2){
+    return -1;
+      }else if (num1 < num2) {
+        return 1;
+      }
+      return 0;
+}
+
+const firstNameIsFirstAlphabetically = (name1,name2) => {
+if(name1 > name2){ //huh?
+return 1
+} else if(name1 < name2) {
+  return -1
+}
+return 0
+}
+
+const firstLastNameIsFirstAlphabetically = (lastName1, lastName2) => {
+  if (lastName2 > lastName1){ //slice method or buildstring with loop //still dont understand slice really
+return 1
+  }
+}
+
+const trueIsFirst = (boolean1,boolean2) => {
+  if (boolean1 > boolean2){
+    return -1
+      }else if (boolean1 < boolean2){
+return 1
+      }
+  return 0
+}
 
 
 
 /************************
 // ITERATION FUNCTIONS
  ************************/
+const sortByNumberAscending = (numbers) => {
+  return numbers.slice().sort(firstNumberIsLess)
+}//slice doesnt modify original array.
+
+const sortByNumberDescending = (numbers) => {
+  return numbers.slice().sort(secondNumberIsLess)
+}
+
+const truesFirst = (array) => {
+  return array.slice().sort(trueIsFirst)
+}
+
+const sortByNameAscending = (names) => {
+  return names.slice().sort()
+}
+
+const sortByLastNameAscending = (lastNames) => {
+  
+}
+
 
 
 
